6ce1175e92337c690bb34fa44175410222923ea7
Nhan <nhantrungha@gmail.com>
2022-03-07 15:55:34 -0800

    document analyzer

---
+++ document_analyzer.py
@@ -0,0 +1,18 @@
+from collections import Counter
+
+data = open("document.txt", "r", encoding="utf8")
+
+data_text = data.read()
+
+split = data_text.split()
+
+Counter = Counter(split)
+
+occurrences = Counter.most_common(5)
+
+output = list(occurrences)
+
+for i in output:
+    print(i)
+
+data.close()
---
+++ sort.py
@@ -0,0 +1,16 @@
+def sort_list(nums: list[int]) -> list[int]:
+    n = len(nums)
+    i = 0
+    j = 0
+
+    while i < n-1:
+        k = n - i - 1
+        j = 0
+        while j < k:
+            if nums[j] > nums[j+1]:
+                nums[j], nums[j+1] = nums[j+1], nums[j]
+            j += 1
+        i += 1
+    return nums
+
+print(sort_list([64, 35, 25, 12, 22, 11 ,90]))
