1111baa5b5c86c3bcf0403e7aedb056f4e1b40cc
Nhan <nhantrungha@gmail.com>
2022-03-08 19:49:50 -0800

    error check

--- document_analyzer.py
+++ document_analyzer.py
@@ -6,10 +6,10 @@ split = data_text.split()
 
 test_dict = {}
 for i in split:
-    if i not in test_dict:
-        test_dict[i] = 1
+    if i.lower() not in test_dict:
+        test_dict[i.lower()] = 1
     else:
-        test_dict[i] += 1
+        test_dict[i.lower()] += 1
 
 output = sorted(test_dict.items(), key=lambda x: x[1])
 output.reverse()
